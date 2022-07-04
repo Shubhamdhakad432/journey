388cc949b8653587574125c027ed19c3fb3d140d
SHUBHAM DHAKAD <shubhamdhakad432@gmail.com>
2022-07-04 18:36:25 +0530

    adding script

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use("/static", express.static("static")); //serve static file
 app.use("/images", express.static("images")); //serve static file
+// app.use("/views", express.static("views")); //serve static file
+app.set("views", path.join(__dirname, "views")); // set view directory
 
 //endpoints (get, post etc)
 app.get("/", (req, res) => {
