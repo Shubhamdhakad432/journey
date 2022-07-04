ef3e8695f236f78bdce374ef0b67120ce072d2f2
SHUBHAM DHAKAD <shubhamdhakad432@gmail.com>
2022-07-04 18:06:23 +0530

    port change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // Express specification stuff
 app.set("view engine", "ejs");
